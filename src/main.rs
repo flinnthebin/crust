a5433f090750e148c87a4ce95abd5da706f5bfc2
Chris Flinn <christopher.flinn@gmail.com>
2024-09-06 03:35:51 +1000

    more work

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-#![no_std]
-#![no_main]
+#![no_std] // disable panic handler
+#![no_main] // disable crt0
 
 use core::panic::PanicInfo;
-
-// default linker behaviour (clang) search for a function named `_start()`
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
